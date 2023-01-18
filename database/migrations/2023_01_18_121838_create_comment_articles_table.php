d04e96064a0c4e96b4e364bc4195aba01f4a349d
Krisna Wahyu Setyawan <exclusive.dec4@gmail.com>
2023-01-18 13:25:03 +0700

    add model and migration forum

--- database/migrations/2023_01_18_121838_create_comment_articles_table.php
+++ database/migrations/2023_01_18_121838_create_comment_articles_table.php
@@ -15,15 +15,6 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->nullable();
-            $table->string('slug');
-            $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('category_id')->constrained('categories');
-            $table->text('summary');
-            $table->longText('body');
-            $table->text('thumbnail');
-            $table->enum('status',['show', 'draft'])->default('draft');
-            $table->unsignedBigInteger('like')->nullable();
             $table->timestamps();
         });
     }
