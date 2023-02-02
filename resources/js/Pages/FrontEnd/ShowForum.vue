4f868e12a83e48fd5d9541557eec59a7f119c05f
alfianaditya <alfianaditya730@gmail.com>
2023-02-02 16:05:10 +0700

    KURANG TAMPILAN YANG KOMEN SAMA LOGO SEND

--- resources/js/Pages/FrontEnd/ShowForum.vue
+++ resources/js/Pages/FrontEnd/ShowForum.vue
@@ -19,101 +19,93 @@ const props = defineProps({
     <br />
     <br />
     <Link :href="route('all-forum.allforum')" class="flex">
-      <p class="flex mx-36 text-xl font-bold">
-        <ChevronLeftIcon class="w-8 h-7" />Back To Article
+      <p class="flex text-black font-medium text-base ml-12 md:text-xl md:ml-24 lg:text-2xl lg:ml-32">
+        <ChevronLeftIcon class="w-5 h-6 md:w-6 md:h-7 lg:w-7 lg:h-8" />Back To Forum
       </p>
     </Link>
     <br />
     <br />
-    <div
-      class="
-        max-w-xs
-        my-auto
-        md:max-w-2xl
-        lg:max-w-5xl
-        mx-auto
-        rounded-xl
-        overflow-hidden
-      "
-      v-for="forum in forums"
-      :key="forum.id"
-    >
-      <div class="md:flex">
-        <div class="p-8">
-          <div class="flex">
-            <UserCircleIcon class="text-sky-900 w-14 h-14"></UserCircleIcon>
-            <div class="mt-1.5">
-              <div class="ml-6 uppercase tracking-wide text-sm font-semibold">
-                {{ forum.user.name }}
-              </div>
-              <p class="ml-6 font-medium text-sm opacity-50">
-                Asked on {{ forum.created_at }} WIB
-              </p>
-              <div
-                class="mt-2 ml-6 font-medium text-2xl mt-7"
-                v-html="forum.subject"
-              ></div>
-              <div class="mt-7">
-                <div class="flex ml-6 text-xl items-center">
-                  <HandThumbUpIcon class="w-6 h-6" />
-                  <span class="mx-2 mr-6">{{ forum.likes.length }} </span>
-
-                  <ChatBubbleLeftEllipsisIcon class="w-6 h-6" />
-                  <span class="mx-2">{{ forum.comments.length }}</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="mt-7 tracking-wide mx-auto py-auto flex-none">
-            <a
-              class="w-full py-2 h-full bg-sky-900 rounded-xl"
-              type=""
-              href="#comment"
-            >
-              <b class="mx-14 text-white md:mx-56 lg:mx-96">Answer Question</b>
-            </a>
-          </div>
-        </div>
-      </div>
+    <div class=" max-w-xs mx-auto overflow-hidden md:max-w-5xl lg:max-w-7xl">
       <div
-        class="
-          max-w-xs
-          md:max-w-2xl
-          lg:max-w-5xl
-          mx-auto
-          rounded-xl
-          overflow-hidden
-        "
-        v-for="comment in comments"
-        v-bind:key="comment.id"
-      >
-        <br />
-        <br />
-
-        <hr class="w-4/5 m-auto border-t-2" />
-
-        <br />
-        <br />
-        <div class="md:flex">
-          <div class="lg:p-8 ml-7">
+        v-for="forum in forums"
+        :key="forum.id"
+        >
+        <div class="">
+          <div class="py-4 md:py-6 md:ml-24 lg:p-8">
             <div class="flex">
-              <UserCircleIcon class="text-sky-900 w-14 h-14"></UserCircleIcon>
+              <UserCircleIcon class="text-sky-900 w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14"></UserCircleIcon>
               <div class="mt-1.5">
-                <div class="ml-6 uppercase tracking-wide text-sm font-semibold">
-                  {{ comment.user.name }}
+                <div class="ml-3 uppercase tracking-wide text-xs text-black font-semibold md:text-base lg:text-lg">
+                  {{ forum.user.name }}
                 </div>
-                <p class="ml-6 font-medium text-sm opacity-50">
-                  Answered On {{ comment.created_at }} WIB
+                <p class="ml-3 font-medium text-slate-400 text-xs md:text-sm lg:text-base">
+                  Asked on {{ forum.created_at }} WIB
                 </p>
                 <div
-                  class="mt-2 ml-6 font-medium text-2xl mt-7"
-                  v-html="comment.comment"
+                  class="mt-2 ml-3 text-black font-medium mt-7 text-base md:text-xl lg:text-2xl"
+                  v-html="forum.subject"
                 ></div>
+                <div class="mt-4 md:mt-6">
+                  <div class="flex ml-3 text-black items-center text-sm md:text-xl">
+                    <HandThumbUpIcon class="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6" />
+                    <span class="mx-2 mr-6">{{ forum.likes.length }} </span>
+
+                    <ChatBubbleLeftEllipsisIcon class="w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6" />
+                    <span class="mx-2">{{ forum.comments.length }}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          
+
+          </div>
+          <div class=" mt-7 flex max-w-xs overflow-hidden mx-auto md:max-w-xl  lg:max-w-3xl">
+              <a
+                class="w-full text-white py-2 h-full bg-sky-900 rounded-xl text-center"
+                type=""
+                href="#comment"
+              >
+                <b class="text-white">Answer Question</b>
+              </a>
+            </div>
+        </div>
+
+        <div
+          v-for="comment in comments"
+          v-bind:key="comment.id"
+          >
+          <br />
+          <br />
+
+          <hr class=" m-auto border-t-2" />
+
+          <br />
+          <br />
+
+          <div>
+            <div class="py-4 md:py-6 md:ml-24 lg:p-8">
+              <div class="flex">
+                <UserCircleIcon class="text-sky-900 w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14"></UserCircleIcon>
+                <div class="mt-1.5">
+                  <div class="ml-6 uppercase tracking-wide text-sm font-semibold">
+                    {{ comment.user.name }}
+                  </div>
+                  <p class="ml-6 font-medium text-sm opacity-50">
+                    Answered On {{ comment.created_at }} WIB
+                  </p>
+                  <div
+                    class="mt-2 ml-6 font-medium text-2xl mt-7"
+                    v-html="comment.comment"
+                  ></div>
+                </div>
               </div>
             </div>
           </div>
+
         </div>
+
       </div>
+
       <br />
       <br />
 
@@ -125,21 +117,18 @@ const props = defineProps({
       <br />
 
       <section id="comment">
-        <div
-          class="
-            max-w-xs
-            md:max-w-2xl
-            lg:max-w-5xl
-            mx-auto
-            rounded-xl
-            overflow-hidden
-          "
-        >
-          <div class="md:flex">
+          <div>
             <div class="lg:p-8 w-full">
-              <div class="flex">
-                <UserCircleIcon class="text-sky-900 w-14 h-14"></UserCircleIcon>
-                <div class="ml-4 pr-1 bg-sky-900 rounded-2xl w-full">
+              <div class="flex max-w-xs overflow-hidden mx-auto md:max-w-xl  lg:max-w-3xl">
+                <UserCircleIcon class="text-sky-900 w-12 h-12 md:w-12 md:h-12 lg:w-14 lg:h-14"></UserCircleIcon>
+                  <input type="text" placeholder="Write Answer..." class="w-full  bg-sky-900 shadow appearance-none border rounded-2xl  py-2 px-3 mt-1  text-white leading-tight focus:outline-none focus:shadow-outline ">
+                <UserCircleIcon class="text-sky-900 w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14"></UserCircleIcon>
+                  <!-- <a href="" class="text-black">Post</a> -->
+                
+              <!-- ngkok gantinen logo Pesawat opo opo ngunu gantine tombol kirim -->
+
+
+                  <!-- <div class="ml-4 pr-1 bg-sky-900 rounded-2xl w-full ">
                   <Textarea
                     class="
                       ml-4
@@ -147,6 +136,7 @@ const props = defineProps({
                       bg-sky-900
                       text-white
                       w-auto
+                      w-11/12
                       md:w-10/12
                       lg:w-11/12
                     "
@@ -155,12 +145,12 @@ const props = defineProps({
                     placeholder="Write Answer"
                     rows="6"
                   ></Textarea>
-                  <a href="" class="text-white">Post</a>
-                </div>
+                  
+                </div> -->
+                
               </div>
             </div>
           </div>
-        </div>
       </section>
     </div>
   </div>
